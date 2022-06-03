af9a3ef09342fbf0a585038d8e9109ff08566b37
ZhEaIsNsAaBn RAGA'I <soona1993@windowslive.com>
2022-06-03 16:24:11 +0200

    PrismLazyLoadingg

---
+++ Src/ZhEaIsNsAaBn.Xamarin/LazyView/IPrismLazyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZhEaIsNsAaBn.Xamarin
+{
+    internal interface IPrismLazyViewModel
+    {
+        void LazyLoadInitialize(INavigationParameters parameters);
+    }
+}
---
+++ Src/ZhEaIsNsAaBn.Xamarin/LazyView/PrismLazyView.cs
@@ -0,0 +1,45 @@
+﻿
+using Prism;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace ZhEaIsNsAaBn.Xamarin
+{
+    public class PrismLazyView<TView> : BaseLazyView
+        where TView : View, new()
+    {
+        public static readonly BindableProperty NavigationParametersProperty = BindableProperty.Create(
+            nameof(NavigationParameters), typeof(INavigationParameters), typeof(PrismLazyView<TView>), BindingMode.TwoWay);
+
+        private void OnNavigationParametersChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            ((IPrismLazyViewModel)Content.BindingContext).LazyLoadInitialize(NavigationParameters);
+        }
+
+        public INavigationParameters NavigationParameters
+        {
+            get => (INavigationParameters)GetValue(NavigationParametersProperty);
+            set
+            {
+                SetValue(NavigationParametersProperty, value);
+                ((IPrismLazyViewModel)Content.BindingContext).LazyLoadInitialize(NavigationParameters);
+            }
+        }
+
+
+        public override ValueTask LoadViewAsync()
+        {
+            var Content = (TView)PrismApplicationBase.Current.Container.Resolve(typeof(TView));
+
+            ((IPrismLazyViewModel)Content.BindingContext).LazyLoadInitialize(NavigationParameters);
+
+            SetIsLoaded(true);
+            return new ValueTask(Task.FromResult(true));
+        }
+    }
+}
